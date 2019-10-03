476fdc424cb303ed10b210c25e4b804e8f313ef0
Ajay Mourya <ajaymourya8553@gmail.com>
2019-10-03 15:00:35 +0530

    created viewpager for result activity

--- app/src/main/java/com/ajaymourya/githubapi/ResultActivity.kt
+++ app/src/main/java/com/ajaymourya/githubapi/ResultActivity.kt
@@ -2,11 +2,19 @@ package com.ajaymourya.githubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ajaymourya.githubapi.adapters.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        setSupportActionBar(result_toolbar)
+
+        val userId:String = intent.getStringExtra("userId")
+
+       
     }
 }
