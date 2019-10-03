4db26324daad45152cbc9dde31e21f20c654f00c
Ajay Mourya <ajaymourya8553@gmail.com>
2019-10-03 14:35:43 +0530

    added search fragment in main activity

--- app/src/main/java/com/ajaymourya/githubapi/MainActivity.kt
+++ app/src/main/java/com/ajaymourya/githubapi/MainActivity.kt
@@ -2,11 +2,21 @@ package com.ajaymourya.githubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ajaymourya.githubapi.search.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SearchFragment.newInstance())
+                .commitNow()
+        }
     }
 }
